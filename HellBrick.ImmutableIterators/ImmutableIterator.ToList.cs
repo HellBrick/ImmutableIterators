4ed886a56d60109db2afa80d7e8e69a8bbbbc4bf
HellBrick <hellbrick@outlook.com>
2018-12-21 15:23:37 +0300

    Added ImmutableIterator.ToList()

---
+++ HellBrick.ImmutableIterators/ImmutableIterator.ToList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace HellBrick.ImmutableIterators
+{
+	public static partial class ImmutableIterator
+	{
+		public static List<T> ToList<T, TState>( this in ImmutableIterator<T, TState> iterator )
+			where TState : struct, IIteratorState<T, TState>
+			=> iterator.Aggregate
+			(
+				new List<T>(),
+				( list, item ) =>
+				{
+					list.Add( item );
+					return list;
+				}
+			);
+	}
+}
