fa1ecc20d1f11e51feaaee6133261acb1c6c84ea
HellBrick <hellbrick@outlook.com>
2018-12-21 15:24:23 +0300

    Added ImmutableIterator.ToArray()

---
+++ HellBrick.ImmutableIterators/ImmutableIterator.ToArray.cs
@@ -0,0 +1,11 @@
+﻿namespace HellBrick.ImmutableIterators
+{
+	public static partial class ImmutableIterator
+	{
+		public static T[] ToArray<T, TState>( this in ImmutableIterator<T, TState> iterator )
+			where TState : struct, IIteratorState<T, TState>
+			=> iterator
+			.ToList()
+			.ToArray();
+	}
+}
